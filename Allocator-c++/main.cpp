41000aba4d968ac296e3ea011e7e8ad4d2a47eb8
KySo4oK <prostorostuk08@gmail.com>
2020-10-15 13:07:53 +0300

    add method structure of allocator

--- Allocator-c++/main.cpp
+++ Allocator-c++/main.cpp
@@ -4,3 +4,11 @@ int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
+
+void *mem_alloc(size_t size);
+
+void *mem_realloc(void *addr, size_t size);
+
+void mem_free(void *addr);
+
+void mem_dump();
