88e37413033382469640e19fa84e6c068dd008a7
KySo4oK <prostorostuk08@gmail.com>
2020-12-14 22:56:54 +0200

    rewrite original code to java

--- memory-optimization/src/main/java/org/example/App.java
+++ memory-optimization/src/main/java/org/example/App.java
@@ -2,5 +2,20 @@ package org.example;
 
 public class App {
     public static void main(String[] args) {
+        System.out.println(System.nanoTime());
+        int[][][] AMD = new int[10][10][10];
+            int res = 0;
+
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    for (int k = 0; k < 10; k++)
+                    {
+                        AMD [k] [j] [i]++;
+                    }
+                }
+            }
+        System.out.println(System.nanoTime());
     }
 }
