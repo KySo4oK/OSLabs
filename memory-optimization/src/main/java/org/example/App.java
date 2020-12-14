4d482b881ecf8e4213559e3217a71a17e0ab191b
KySo4oK <prostorostuk08@gmail.com>
2020-12-14 23:03:39 +0200

    replace 10 to 100 in arrays bound's for better obviousness and add printing difference in nano times

--- memory-optimization/src/main/java/org/example/App.java
+++ memory-optimization/src/main/java/org/example/App.java
@@ -2,20 +2,23 @@ package org.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println(System.nanoTime());
-        int[][][] AMD = new int[10][10][10];
+        long nanoTimeBefore = System.nanoTime();
+        System.out.println(nanoTimeBefore);
+        int[][][] AMD = new int[100][100][100];
             int res = 0;
 
-            for (int i = 0; i < 10; i++)
+            for (int i = 0; i < 100; i++)
             {
-                for (int j = 0; j < 10; j++)
+                for (int j = 0; j < 100; j++)
                 {
-                    for (int k = 0; k < 10; k++)
+                    for (int k = 0; k < 100; k++)
                     {
                         AMD [k] [j] [i]++;
                     }
                 }
             }
-        System.out.println(System.nanoTime());
+        long nanoTimeAfter = System.nanoTime();
+        System.out.println(nanoTimeAfter);
+        System.out.println(nanoTimeAfter-nanoTimeBefore);
     }
 }
